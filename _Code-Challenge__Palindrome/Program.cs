e4e33642aa53946c169c781671c8692eb8f3b197
Eric L. Hepperle <codewizard13@gmail.com>
2026-06-29 08:29:32 -0500

    docs(exercises): finalize ch4 `functions` code challenge (Palindrome tester)

--- _Code-Challenge__Palindrome/Program.cs
+++ _Code-Challenge__Palindrome/Program.cs
@@ -6,47 +6,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        StringBuilder sb = new StringBuilder("Initial String", 200);
+        StringBuilder sb = new StringBuilder("A man, a plan, a canal, Panama", 200);
 
+        // TESTING
         HandleMaybePalindrome(sb.ToString());
-
+        HandleMaybePalindrome("madam");
+        HandleMaybePalindrome("Madame");
+        HandleMaybePalindrome("Car lot");
     }
 
     /* Function: determine if a string is a palindrome */
-    public static bool IsPalindrome(String thestr)
+    public static bool IsPalindrome(string thestr)
     {
         //* STEP 1: Normalize the string (removing whitespace and punctuation) *
-        // // Rebuild cleaned string using StringBuilder 
-        StringBuilder sb = new StringBuilder(thestr);
+        StringBuilder sb = new StringBuilder();
 
         // Loop through and remove disallowed characters
-        foreach (Char c in thestr)
+        foreach (char c in thestr)
         {
-            if (!Char.IsPunctuation(c) && !Char.IsWhiteSpace(c))
+            if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
             {
-                sb.Append(Char.ToLower(c));
+                sb.Append(char.ToLower(c));
             }
         }
 
-        // Verify cleaned string 
-        Console.WriteLine($"Cleaned string: \n");
-        Console.WriteLine(sb.ToString());
-        
-        //* STEP 2: Test cleaned string for palindromicity *
-        string s = sb.ToString(); int leftIndex = 0; int rightIndex = s.Length - 1; while (leftIndex < rightIndex)
+        string s = sb.ToString();
+        int leftIndex = 0;
+        int rightIndex = s.Length - 1;
+
+        while (leftIndex < rightIndex)
         {
-            // Check if check if c = s[right] 
             if (s[leftIndex] != s[rightIndex])
             {
-                // Early return 
                 return false;
             }
-            else
-            {
-                // Increment left, decrement right 
-                leftIndex++; rightIndex--;
-            }
+
+            leftIndex++;
+            rightIndex--;
         }
+
         return true;
     }
 
@@ -54,11 +52,11 @@ class Program
     {
         if (IsPalindrome(s.ToString()))
         {
-            Console.WriteLine("Yep, the string is a palindrome.");
+            Console.WriteLine($"Yep, the string {{{s}}} is a palindrome.");
         }
         else
         {
-            Console.WriteLine("NOPE - It's NOT a palindrome!");
+            Console.WriteLine($"NOPE - {{{s}}} is NOT a palindrome!");
         }
     }
 
